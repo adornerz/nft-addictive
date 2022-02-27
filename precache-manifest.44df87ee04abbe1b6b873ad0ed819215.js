fc15ca120c64627fdde17b13bf236ba36da44627
adornerz <adors@protonmail.com>
2022-02-27 09:59:25 +0100

    Updates

---
+++ precache-manifest.44df87ee04abbe1b6b873ad0ed819215.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "239ba1e9674548f5de1db879e44bd749",
+    "url": "/nft-addictive/index.html"
+  },
+  {
+    "revision": "92b979932acd434b8668",
+    "url": "/nft-addictive/static/css/2.f1b3b41f.chunk.css"
+  },
+  {
+    "revision": "6caa53a521474b2f1414",
+    "url": "/nft-addictive/static/css/main.0cebbf21.chunk.css"
+  },
+  {
+    "revision": "92b979932acd434b8668",
+    "url": "/nft-addictive/static/js/2.33bcb209.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/nft-addictive/static/js/2.33bcb209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6caa53a521474b2f1414",
+    "url": "/nft-addictive/static/js/main.fa5f6268.chunk.js"
+  },
+  {
+    "revision": "f9259b6d435155b17ba0",
+    "url": "/nft-addictive/static/js/runtime-main.518f76da.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/nft-addictive/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/nft-addictive/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/nft-addictive/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/nft-addictive/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "6fd2e69b45e321b7a84fd3c4ab25f156",
+    "url": "/nft-addictive/static/media/hero-image.6fd2e69b.jpg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/nft-addictive/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/nft-addictive/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "8af92279cea18abd5ac870f175e904ce",
+    "url": "/nft-addictive/static/media/logo.8af92279.jpeg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/nft-addictive/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/nft-addictive/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/nft-addictive/static/media/revicons.2feb69cc.eot"
+  }
+]);
